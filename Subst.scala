dc289fe6fb48e1017e492bbdfb752d00afa7e9c4
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-03-09 22:45:40 +0100

    Fix indentation

--- Subst.scala
+++ Subst.scala
@@ -62,39 +62,40 @@ object Lang extends TypedTraversal {
   }
 
   def subst[S, T](v: Var[T], repl: Exp[T])(t: Exp[S]): Exp[S] =
-      t match {
-        //Cool! Same insight as in the other interpreter. Note that if I just
-        //match names, it won't typecheck.
-        case _: v.type =>
-          repl
-        case w: Var[t] =>
-          assert(v.name != w.name)
+    t match {
+      //Cool! Same insight as in the other interpreter. Note that if I just
+      //match names, it won't typecheck.
+      case _: v.type =>
+        repl
+      case w: Var[t] =>
+        assert(v.name != w.name)
+        t
+      case app: App[s, t] =>
+        val fun = app.f
+        val arg = app.e
+        //I'd like to have a recursive subfunction taking just t,
+        //but it's hard to write because it should also take S!
+        App[s, t](subst(v, repl)(fun), subst(v, repl)(arg))
+      case l: Lam[s, t] =>
+        val w = l.x
+        val body = l.e
+        if (w eq v) //Must check identity, for coherency with variable lookup.
           t
-        case app: App[s, t] =>
-          val fun = app.f
-          val arg = app.e
-          //I'd like to have a recursive subfunction taking just t,
-          //but it's hard to write because it should also take S!
-          App[s, t](subst(v, repl)(fun), subst(v, repl)(arg))
-        case l: Lam[s, t] =>
-          val w = l.x
-          val body = l.e
-          if (w eq v) //Must check identity, for coherency with variable lookup.
-            t
-          else {
-            //This only works if w is not free in repl.
-            //Otherwise, we need to use another w.
-            //Lam(w, subst(v, repl)(body))
+        else {
+          //This only works if w is not free in repl.
+          //Otherwise, we need to use another w.
+          //Lam(w, subst(v, repl)(body))
 
-            //To avoid implementing freeVars, we just do another
-            //substitution with a free variable.
-            val x = freshVar[s]()
-            val alphaRenamedBody = subst(w, x)(body)
-            Lam[s, t](x, subst(v, repl)(alphaRenamedBody))
-          }
-        case Num(_) => t
-        case Succ => t
-      }
+          //To avoid implementing freeVars, we just do another
+          //substitution with a free variable.
+          val x = freshVar[s]()
+          val alphaRenamedBody = subst(w, x)(body)
+          Lam[s, t](x, subst(v, repl)(alphaRenamedBody))
+        }
+      case Num(_) => t
+      case Succ => t
+    }
+
   def betaReduce[S, T](f: Lam[S, T], arg: Exp[S]): Exp[T] =
     subst(f.x, arg)(f.e)
 
